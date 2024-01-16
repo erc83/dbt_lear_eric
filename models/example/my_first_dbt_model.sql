c2f1b8f00299a87b76e0be1737fb41e99f856a20
eric_espinoza <espinoza.eric@gmail.com>
2024-01-16 01:19:21 -0300

    advanced topics, snapshot and source data

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,11 +9,12 @@
 {{ config(materialized='table', alias='first_model', post_hook='grant select on {{this}} to role analyst') }}
 
 with source_data as (
-    select 1 as id
+    -- select 1 as id, 'CA' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    select 1 as id, 'NY' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
     union all
-    select null as id
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
     union all
-    select 3 as id 
+    select 3 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 )
 
 select *
